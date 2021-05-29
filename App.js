7768fe8e206e78d6c7863d93eb66b7dc3a8eb193
nutlope <he55@drexel.edu>
2021-05-28 21:48:34 -0400

    added state with toggle

--- App.js
+++ App.js
@@ -1,22 +1,32 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, View, Switch } from "react-native";
 
 export default function App() {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+
   return (
     <View style={styles.container}>
-      <Text>This is cool!</Text>
-      <TextInput
-        style={{
-          height: 40,
-          width: 200,
-          borderColor: "green",
-          marginTop: 20,
-          borderWidth: 3,
-        }}
-        defaultValue="You can type in me"
-      />
-      <StatusBar style="auto" />
+      <Text style={styles.title}>EatNow</Text>
+      <View style={styles.side}>
+        <Text style={{ fontSize: 22 }}>Remind me to eat</Text>
+        <Switch
+          trackColor={{ false: "#767577", true: "#81b0ff" }}
+          // thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={isEnabled}
+        />
+      </View>
+      {isEnabled && <Text>Ok I'll remind you</Text>}
+      {!isEnabled && <Text>Fine I won't remind you</Text>}
+
+      <View style={{ borderWidth: 2, padding: 10, margin: 30 }}>
+        <Text style={{ fontSize: 18, marginTop: 20 }}>
+          Type something below
+        </Text>
+        <TextInput style={styles.input} defaultValue="You can type in me" />
+      </View>
     </View>
   );
 }
@@ -26,6 +36,23 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
+  },
+  title: {
+    fontSize: 70,
+    marginBottom: 150,
+    marginTop: 50,
+  },
+  input: {
+    height: 40,
+    width: 250,
+    borderColor: "green",
+    marginTop: 20,
+    borderWidth: 2,
+  },
+  side: {
+    width: "100%",
+    height: 60,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
   },
 });
