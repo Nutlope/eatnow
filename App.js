49a56ad436119c8bfd3a12ed8a295aa2854383c9
nutlope <he55@drexel.edu>
2021-05-28 21:12:14 -0400

    added basic components

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is cool!</Text>
+      <TextInput
+        style={{
+          height: 40,
+          width: 200,
+          borderColor: "green",
+          marginTop: 20,
+          borderWidth: 3,
+        }}
+        defaultValue="You can type in me"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
